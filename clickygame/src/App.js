0e347fd9ab31bc5a36f3e0354c5dfe821d72fe83
haog29 <hgagaring@yahoo.com>
2019-06-22 13:46:41 -0700

    initial Commit and Created components

---
+++ clickygame/src/App.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react'
+import Navbar from './Components/Navbar/Navbar.js'
+import Jumbotron from './Components/Jumbotron/Jumbotron.js'
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <>
+      < Navbar />
+      < Jumbotron />
+
+<div className= "container" > 
+    <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+  <div className="btn-group mr-2" role="group">
+    <button type="button" className="button1"></button>
+ 
+  </div>
+  <div className="btn-group mr-2" role="group">
+    <button type="button" className="button2"></button>
+
+  </div>
+  <div className="btn-group" role="group" >
+    <button type="button" className="button3"></button>
+
+  </div>
+</div>
+
+<div className="btn-toolbar" role="toolbar">
+  <div className="btn-group mr-2" role="group">
+    <button type="button" className="button4"></button>
+ 
+  </div>  
+  <div className="btn-group mr-2" role="group" >
+    <button type="button" className="button5"></button>
+
+  </div>
+  <div className="btn-group" role="group">
+    <button type="button" className="button6"></button>
+
+  </div>
+</div>
+<div className="btn-toolbar" role="toolbar" >
+  <div className="btn-group mr-2" role="group" >
+    <button type="button" className="button7"></button>
+ 
+  </div>
+  <div className="btn-group mr-2" role="group" >
+    <button type="button" className="button8"></button>
+
+  </div>
+  <div className="btn-group" role="group" >
+    <button type="button" className="button9"></button>
+
+  </div>
+</div>
+<div className="btn-toolbar" role="toolbar" >
+  <div className="btn-group mr-2" role="group">
+    <button type="button" className="button10"></button>
+ 
+  </div>
+  <div className="btn-group mr-2" role="group" >
+    <button type="button" className="button11"></button>
+
+  </div>
+  <div className="btn-group" role="group">
+    <button type="button" className="button12"></button>
+    </div>
+  </div>
+</div>
+      </>
+    )
+  }  
+
+}
+export default App;
---
+++ clickygame/src/Components/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Jumbotron = _ => {
+    return (
+        <>
+
+ <div className="jumbotron">
+  <h1 className="display-4">BT21 Clicky Game!</h1>
+  <hr className="my-4" />
+  <p className="lead">Click on an Character to begin playing. But don't click on the same morethan once! </p>
+
+  </div>
+
+        </>
+    )
+}
+
+export default Jumbotron
